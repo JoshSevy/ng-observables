13ef2e5655cf8627990434328b993b0605fd8a4b
JoshSevy <joshuasevy@outlook.com>
2020-12-17 18:56:18 -0700

    Updated app to use services from event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub: Subscription;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(wasActivated => {
+      this.userActivated = wasActivated;
+    })
+  }
+
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
 }
