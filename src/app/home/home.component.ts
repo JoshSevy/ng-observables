f9718f331c61356c0d3ed80104f54e2b38688be2
JoshSevy <joshuasevy@outlook.com>
2020-12-17 17:42:23 -0700

    Added custom build observable in project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { interval } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
-  ngOnInit() {
-    interval(1000).subscribe(count => {
-      console.log(count);
+  ngOnInit(): void {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count > 5) observer.complete();
+        count++;
+      }, 1000);
     });
+
+    this. firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data)
+    }, error => {
+      console.log(error.message);
+    }, () => {
+      console.log('Completed!')
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+
   }
 
 }
